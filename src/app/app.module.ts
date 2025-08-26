f056e4a5cb0a0519fc2bdc180f08a0f14a539b53
Shashank <lshashank414@gmail.com>
2025-08-26 14:42:16 +0530

    angular-18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ToastrModule } from 'ngx-toastr';
@@ -20,46 +20,40 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 
 
 declare const _IntegrationMediumOffice:boolean;
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CommonModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    // 3rdParty module
-    AuthModule,
-    // core & shared
-    CoreModule,
-    FormsModule,
-    SharedModule,
-    HttpClientModule,
-    ImageCropperModule,
-    //ToastrCongfiguraion
-    ToastrModule.forRoot({
-      timeOut: 5000, // 5 seconds
-      closeButton: true,
-      progressBar: false,
-    }),
-    HomeRoutingModule,
-    UserSettingsRoutingModule,
-    DragDropModule,
-  ],
-  providers: [
-    DecimalPipe,
-    AccountService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptor,
-      multi: true
-    },
-    {
-      provide: LocationStrategy,
-      useClass: _IntegrationMediumOffice ? HashLocationStrategy : PathLocationStrategy,
-    },
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        CommonModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        // 3rdParty module
+        AuthModule,
+        // core & shared
+        CoreModule,
+        FormsModule,
+        SharedModule,
+        ImageCropperModule,
+        //ToastrCongfiguraion
+        ToastrModule.forRoot({
+            timeOut: 5000, // 5 seconds
+            closeButton: true,
+            progressBar: false,
+        }),
+        HomeRoutingModule,
+        UserSettingsRoutingModule,
+        DragDropModule], providers: [
+        DecimalPipe,
+        AccountService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthInterceptor,
+            multi: true
+        },
+        {
+            provide: LocationStrategy,
+            useClass: _IntegrationMediumOffice ? HashLocationStrategy : PathLocationStrategy,
+        },
+        provideHttpClient(withInterceptorsFromDi()),
+    ] })
 export class AppModule { }
